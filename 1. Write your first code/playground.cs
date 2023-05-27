0454f60e1672eda04d9f1272660046e69de81191
Jhon C. Valencia <carlosmcms725@hotmail.com>
2023-05-27 16:19:05 -0500

    1. Write your first code: Completed

---
+++ 1. Write your first code/playground.cs
@@ -0,0 +1,5 @@
+// Unit 2 of 7: Exercise - Write your first code
+Console.WriteLine("Hello World!");
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
